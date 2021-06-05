4731d8babc42045b9c21e4b9b9e8350392b91791
Aditya Singh <adityasingh27072746@gmail.com>
2021-06-05 15:59:20 +0530

    Got all the resources and completed Video 1 on Reading image and video

---
+++ opencv/read.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+# Reading Image
+
+# img = cv.imread('.\Resources\Photos\cat.jpg')
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
+
+# Reading Video`x
+
+# vid = cv.VideoCapture('.\Resources\Videos\dog.mp4')
+
+# while True:
+#     isTrue, frame = vid.read()
+#     cv.imshow('Video', frame)
+
+#     if cv.waitKey(20) & 0xFF == ord('d'):
+#         break
+
+# vid.release()
+# cv.destroyAllWindows()
+
